1bc5e98743e9bc4f595a53004c3d37e7456e167e
Nathan Reese <github@sect.net>
2021-02-20 21:17:20 -0500

    just typin in the tutorials and learning about components

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <h1>To-Do List</h1>
+    <to-do-form @todo-added="addToDo"></to-do-form>
+     <ul>
+      <li v-for="goblin in ToDoItems" :key="goblin.id">
+       <to-do-item :label="goblin.label" :done="goblin.done" :id="goblin.id"></to-do-item>
+      </li>
+     </ul>
+  </div>
+</template>
+
+<script>
+  import uniqueId from 'lodash.uniqueid';
+  import ToDoItem from './components/Sleestak.vue';
+  import ToDoForm from './components/ToDoForm.vue';
+  export default {
+    name: 'App',
+    components: {
+      ToDoItem,
+      ToDoForm
+    },
+    data() {
+      return {
+        ToDoItems: [
+          { id: uniqueId('todo-'), label: 'Eat lunch', done: true },
+          { id: uniqueId('todo-'), label: 'Solve a myster', done: false },
+          { id: uniqueId('todo-'), label: 'Narfle the Garthok', done: false}
+        ]
+      };
+    },
+    methods :{
+      addToDo(toDoLabel) {
+        console.log('ToDoAdded: ', toDoLabel);
+        this.ToDoItems.push({id:uniqueId('todo-'), label: toDoLabel, done: false});
+      }
+    }
+  };
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+
+}
+ul {
+  list-style-type: none;
+}
+</style>
---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,60 @@
+<template>
+    <form @submit.prevent="onSubmit">
+        <label class="bugs" for="new-todo-input">
+        What needs doin?
+        </label><br>
+        <input
+            type="text"
+            id="new-todo-input"
+            name="new-todo"
+            autocomplete="off"
+            v-model.lazy.trim="label"
+        />
+        <button type="submit">
+            Add
+        </button>
+    </form>    
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                label: ""
+            }
+        },
+        methods: {
+            onSubmit(){
+                if(this.label === "") {
+                    console.log("Refusing to add empty items to the list.");
+                    return;
+                }
+                console.log('Todoform component: ', this.label);
+                this.$emit('todo-added', this.label);
+                this.label = "";
+            }
+        }
+    };
+
+</script>
+// style scoped to this component.
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.bugs {
+    color: red;
+    font-weight: bold;
+}
+</style>
